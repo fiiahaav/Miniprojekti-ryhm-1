66a4e0cca9c319108291165a654f7afad0bb4be5
bjornKhermik10 <bjorn.khermik@helsinki.fi>
2025-11-17 13:20:53 +0200

    html templateja lisää/muokattu

--- src/app.py
+++ src/app.py
@@ -1,7 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import psycopg2
 
 app = Flask(__name__)
 
+# Yhteys tietokantaan
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="maisterityo",
+        user="bjorn",
+        password="salasana"
+    )
+    return conn
+
+#Tässä toi landing page lista jossa lähteet jne
 @app.route("/")
 def index():
-    return render_template("index.html")
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM authors ORDER BY id")
+    authors = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template("index.html", authors=authors)
+
+#Täällä voi lisätä lähteitä
+@app.route("/add", methods=["GET", "POST"])
+def add_author():
+    if request.method == "POST":
+        nimi = request.form["name"]
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO authors (name) VALUES (%s)", (nimi,))
+        conn.commit()
+        cur.close()
+        conn.close()
+        return redirect(url_for('index'))
+    return render_template("add.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+    
